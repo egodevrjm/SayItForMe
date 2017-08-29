f7f64295f76e961b232259c8f2001a4006ad942c
Ryan Morrison <upyourego@icloud.com>
2017-08-29 23:33:51 +0100

    Say It For Me - text to speech in Swift
    A simple text to speech app written in Swift. Available on the App Store.

--- SayItForMe/ViewController.swift
+++ SayItForMe/ViewController.swift
@@ -7,18 +7,84 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    
+    // import the text field
+    @IBOutlet var txtField: UITextView!
+    
+    // set the speaking speed
+    var utteranceRate = 0.5
+    
+    // declare variables to set the whole thing up
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // remove the keyboard on tap
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
+   
+    // play the text field contents
+    @IBAction func playBtn(_ sender: UIButton) {
+        
+        myUtterance = AVSpeechUtterance(string: txtField.text)
+        myUtterance.rate = Float(utteranceRate)
+        synth.speak(myUtterance)
+        
+    }
+    
+    // stop the text field chatter
+    @IBAction func stopBtn(_ sender: UIButton) {
+        
+        synth.stopSpeaking(at: .immediate)
+        
+    }
+    
+    @IBAction func btn1(_ sender: UIButton) {
+        
+        myUtterance = AVSpeechUtterance(string: "I've lost my voice")
+        myUtterance.rate = Float(utteranceRate)
+        synth.speak(myUtterance)
+        
+    }
+    
+    @IBAction func btn2(_ sender: UIButton) {
+        
+        myUtterance = AVSpeechUtterance(string: "Hi, how are you?")
+        myUtterance.rate = Float(utteranceRate)
+        synth.speak(myUtterance)
+        
+    }
+    
+    @IBAction func btn3(_ sender: UIButton) {
+        
+        myUtterance = AVSpeechUtterance(string: "What's for dinner?")
+        myUtterance.rate = Float(utteranceRate)
+        synth.speak(myUtterance)
+        
+    }
+    
+    @IBAction func btn4(_ sender: UIButton) {
+        
+        myUtterance = AVSpeechUtterance(string: "Can you help me?")
+        myUtterance.rate = Float(utteranceRate)
+        synth.speak(myUtterance)
+        
+    }
+    
 
 
 }
